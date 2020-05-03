a2b1c471e1642a96c9a531db4bd8357be4f086ca
Jens Ravens <jens@nerdgeschoss.de>
2020-05-03 21:37:41 +0200

    documentation of DB

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export { DB } from './db';
 export { MutableDB } from './mutable_db';
-export type { DataTable } from './table';
+export type { DataTable, Table } from './table';
 export type { DBAction } from './actions';
+export { Subscription } from './subscription';
+export { Query } from './query';
 export { reducer } from './reducer';
 export { guid, emptyTable } from './util';
